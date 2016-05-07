b98d4bb9fd4251729434fc30379d330083617f83
Denis P <lsden1995@gmail.com>
2016-05-07 18:47:52 +0300

    Make fixes for routings and carousel

--- app/main.js
+++ app/main.js
@@ -1,10 +1,13 @@
-System.register(['rxjs/Rx', "angular2/platform/browser", 'angular2/http', "./components/app/app.component", 'angular2/router'], function(exports_1, context_1) {
+System.register(['rxjs/Rx', 'angular2/core', "angular2/platform/browser", 'angular2/http', "./components/app/app.component", 'angular2/router'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
-    var browser_1, http_1, app_component_1, router_1;
+    var core_1, browser_1, http_1, app_component_1, router_1;
     return {
         setters:[
             function (_1) {},
+            function (core_1_1) {
+                core_1 = core_1_1;
+            },
             function (browser_1_1) {
                 browser_1 = browser_1_1;
             },
@@ -18,7 +21,8 @@ System.register(['rxjs/Rx', "angular2/platform/browser", 'angular2/http', "./com
                 router_1 = router_1_1;
             }],
         execute: function() {
-            browser_1.bootstrap(app_component_1.AppComponent, [http_1.HTTP_PROVIDERS, router_1.ROUTER_PROVIDERS]);
+            browser_1.bootstrap(app_component_1.AppComponent, [http_1.HTTP_PROVIDERS, router_1.ROUTER_PROVIDERS,
+                core_1.provide(router_1.LocationStrategy, { useClass: router_1.HashLocationStrategy })]);
         }
     }
 });
--- app/main.ts
+++ app/main.ts
@@ -1,7 +1,9 @@
 import 'rxjs/Rx';
+import { provide } from 'angular2/core';
 import { bootstrap } from "angular2/platform/browser";
 import { HTTP_PROVIDERS } from 'angular2/http';
 import { AppComponent } from "./components/app/app.component";
-import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy } from 'angular2/router';
 
-bootstrap(AppComponent, [HTTP_PROVIDERS, ROUTER_PROVIDERS]);
+bootstrap(AppComponent, [HTTP_PROVIDERS, ROUTER_PROVIDERS,
+  provide(LocationStrategy, {useClass: HashLocationStrategy})]);
